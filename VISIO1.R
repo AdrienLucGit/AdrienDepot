13a34703e9fee5f299ddc81648b938b46735a99d
Adrien LUC <luc.adrien03@gmail.com>
2025-02-04 13:07:39 +0100

    Changement du dernier graph

--- VISIO1.R
+++ VISIO1.R
@@ -8,7 +8,8 @@ colSums(is.na(starwars)) #pour voir ou il a des NA
 install.packages("ggplot2")
 library(ggplot2)
 
-#enlever les NA
+###enlever les NA des données et autres testes###
+
 data <- starwars |> filter(!is.na(height) & !is.na(mass) & !is.na(hair_color)& !is.na(birth_year) & !is.na(sex)& !is.na(gender)& !is.na(homeworld)& !is.na(species))
 head(data)
 data2 <- data |> select(height, species, homeworld) #pour avoir que 3 variable
@@ -16,25 +17,33 @@ data2
 ggplot(data2) + geom_histogram(aes(x= height)) # histo de la taille
 
 ###chatgpt###
+
 ggplot(data, aes(x = homeworld, y = height, color = species)) +
   geom_point(size = 4, alpha = 0.7) +  
-  theme_minimal() +
+  theme_minimal() + #ca sert a rien autant ecrire theme() et faire le travail
   coord_flip() + # Tourne le graphique pour lisibilité
   ggtitle("Taille des personnages selon leur planète d'origine") +
   theme(plot.title = element_text(hjust = 0.5, face = "bold", size = 14))
 
+
 ###De moi###
-ggplot(data2)+
+ggplot(data)+
   geom_point(aes(x = height, y = homeworld, color= species))
 
 #je veux un graphique de densité 2d avec les especes en point de couleur
 #pour voir la répartition des especes selon height et birth_year
 
-ggplot(data)+
-  geom_density_2d(aes(x = height, y = birth_year))
- # geom_point(aes(color = species),size = 3, alpha = 0.7)+
-  #theme_minimal() +
-  #ggtitle("Densité des tailles et années de naissance avec espèces colorées") +
-  #theme(plot.title = element_text(hjust = 0.5, face = "bold", size = 14))
+
+ggplot (data)+
+  geom_density_2d(aes(x = height, y = birth_year), color = "yellow", linetype = 8)+ #densité 2d, couleure jaune
+  geom_point(aes(x = height, y = birth_year, color = species),size = 3, alpha = 2, shape = 7)+ # points de couleure pour espèces
+  theme(
+    panel.background = element_rect(fill = "black"), #le fond du graphique en noir
+    plot.background = element_rect(fill = "black"), # l'arriere du graphique en noir 
+    axis.text = element_text(color = "white"), #texte = les epeces en blanc
+    axis.title = element_text(color = "white"), # nom des axes en blans
+    plot.title = element_text(hjust = 0.5, face = "bold", size = 12, color = "yellow"))+ #taille et couleur du titre 
+  ggtitle("Densité des tailles et années de naissance des espèces colorées")
+
 
 
